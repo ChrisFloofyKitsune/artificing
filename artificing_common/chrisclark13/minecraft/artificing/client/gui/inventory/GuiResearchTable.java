a3c05ceacc8193e6ef4f328ab83bf9648d631320
cyntain <oliver1.harvey@gmail.com>
2013-05-22 20:37:23 +0100

    [Cyntain] Added the gui for the Research Table, and the name in the .xml; Currently working on binding the rings.png to the gui. Currently does not work, I think i have done it wrong. I'll try some more later.

--- artificing_common/chrisclark13/minecraft/artificing/client/gui/inventory/GuiResearchTable.java
+++ artificing_common/chrisclark13/minecraft/artificing/client/gui/inventory/GuiResearchTable.java
@@ -1,10 +1,12 @@
 package chrisclark13.minecraft.artificing.client.gui.inventory;
 
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.StatCollector;
 
 import org.lwjgl.opengl.GL11;
 
+import chrisclark13.minecraft.artificing.client.renderer.ArtificingRenderHelper;
 import chrisclark13.minecraft.artificing.inventory.ContainerArtificingTable;
 import chrisclark13.minecraft.artificing.inventory.ContainerResearchTable;
 import chrisclark13.minecraft.artificing.tileentity.TileResearchTable;
@@ -15,6 +17,7 @@ public class GuiResearchTable extends GuiMultiSlotItem {
     
     public GuiResearchTable(InventoryPlayer inventoryPlayer, TileResearchTable ResearchTable) {
         super(new ContainerResearchTable(inventoryPlayer, ResearchTable));
+        xSize = 199;
         ySize = 226;
     }
 
@@ -27,16 +30,32 @@ public class GuiResearchTable extends GuiMultiSlotItem {
         fontRenderer.drawString(
                 StatCollector.translateToLocal("container.inventory"), 8,
                 ySize - 96 + 2, 0x404040);
+        
     }
+
+    
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float par1, int par2,
             int par3) {
+        /* Does not currently render the first ring */
+        Tessellator tessellator = Tessellator.instance;
+        GL11.glPushMatrix();
+        GL11.glTranslatef(0.5f, 0.5f, 0);         
+        mc.renderEngine.bindTexture("/mods/artificing/textures/gui/rings.png");
+        tessellator.addVertexWithUV(0, 0, 0, 0, 0);
+        tessellator.addVertexWithUV(0, 0, 0, 0, 1);
+        tessellator.addVertexWithUV(0, 0, 0, 0.5, 1);
+        tessellator.addVertexWithUV(0, 0, 0, 1, 0.5);
+        GL11.glTranslatef(-0.5F, -0.5f, 0);   
+        GL11.glPopMatrix();
+       
+        
         // draw your Gui here, only thing you need to change is the path
         // int texture = mc.renderEngine.getTexture("/gui/trap.png");
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         mc.renderEngine
-                .bindTexture("/mods/artificing/textures/gui/artificingTable.png"); //temp
+                .bindTexture("/mods/artificing/textures/gui/researchTable.png"); 
         // this.mc.renderEngine.bindTexture(texture);
         int x = (width - xSize) / 2;
         int y = (height - ySize) / 2;
