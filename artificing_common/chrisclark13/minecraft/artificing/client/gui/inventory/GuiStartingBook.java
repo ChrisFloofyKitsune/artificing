0f502bde00cadeaf8e0f58b822aabca0e204825b
cyntain <oliver1.harvey@gmail.com>
2013-05-28 20:37:47 +0100

    [cyntain] Guides.

--- artificing_common/chrisclark13/minecraft/artificing/client/gui/inventory/GuiStartingBook.java
+++
@@ -1,9 +0,0 @@
-package chrisclark13.minecraft.artificing.client.gui.inventory;
-
-import net.minecraft.client.gui.GuiScreen;
-
-
-
-public class GuiStartingBook extends GuiScreen {
-
-}
---
+++ artificing_common/chrisclark13/minecraft/artificing/client/gui/inventory/guidepages/GuiPageGuideManual.java
@@ -0,0 +1,11 @@
+package chrisclark13.minecraft.artificing.client.gui.inventory.guidepages;
+
+import chrisclark13.minecraft.artificing.client.gui.GuiContent;
+import net.minecraft.client.gui.GuiScreen;
+
+
+public class GuiPageGuideManual extends GuiGuideBook {
+
+    private GuiContent content;
+
+}
